b760faf0fa9a4e963818bac56e58b58e0adb040e
Marcelo <jsdevmarcelo@gmail.com>
2024-10-24 14:27:05 -0100

    api response chnaged from header to data as object key name

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -5,10 +5,10 @@ export const api = {
     getHomeContent: async () => {
         try {
             const response = await client.get('/home');
-            return response.data; // you are supposed to receive an json "data": {"header": "header.mp4"}
+            return response.data; // you are supposed to receive an json "data": {"value": "header.mp4"}
         } catch (error) {
             console.log('Error', error);
-            return {data: {header: '../../assets/default/plane.png'}}
+            return {data: {value: '../../assets/default/plane.png'}}
         }
     }
 }

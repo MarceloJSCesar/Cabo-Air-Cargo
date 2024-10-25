d68923d899b437df7feeb47dd819eb9a2c2bf22d
Marcelo <jsdevmarcelo@gmail.com>
2024-10-25 09:36:40 -0100

    shadow applied, image centralized

--- src/components/navbar/LeftContent.js
+++ src/components/navbar/LeftContent.js
@@ -1,10 +1,19 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, Image } from "@chakra-ui/react";
 import logo from '../../assets/default/logo.png';
 
 function LeftContent() {
     return (
-       <Flex alignItems='center' justifyContent='space-between' gap={4}>
-            <img src={logo} width='100px' height='100px'/>
+       <Flex 
+        alignItems='center' 
+        justifyContent='space-between' 
+        gap={4}
+       >
+            <Image 
+                src={logo} 
+                width='100px' 
+                height='100px'
+                fit='fill'
+            />
        </Flex>
     );
 }

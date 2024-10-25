2debf155b2b9415b55ea0288d9ea7bdebfcae5e8
Marcelo <jsdevmarcelo@gmail.com>
2024-10-25 00:13:55 -0100

    basic UI created

--- src/views/layout/Header.js
+++ src/views/layout/Header.js
@@ -1,15 +1,52 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, Box } from "@chakra-ui/react";
 import RightContent from "../../components/navbar/RightContent";
 import LeftContent from "../../components/navbar/LeftContent";
+import backgroundImg from "../../assets/default/plane.png";
 
 function Header() {
+
+    const navItems = [
+        {
+            name: 'Home',
+            href: '#',
+        },
+        {
+            name: 'How it works',
+            href: '#',
+        },
+        {
+            name: 'About',
+            href: '#',
+        },
+        {
+            name: 'Contact',
+            href: '#',
+        },
+    ];
+
     return (
-        <HStack w='full' p={2} display='flex' alignItems='center' justifyContent='space-between'>
-            
-            <LeftContent />
-            
-            <RightContent />
-        </HStack>
+            <Box
+                bgImage={`url(${backgroundImg})`}
+                bgSize='cover'
+                bgPos='center'
+                bgRepeat='no-repeat'
+                h={{base: '100vh', md: '80vh'}}
+                minW='100vw'
+            >
+                <HStack 
+                w='full' p={2} 
+                display='flex' 
+                alignItems='center' 
+                justifyContent='space-between' 
+            >
+                
+                    <LeftContent />
+                    
+                    <RightContent navItems={navItems}/>
+
+                    <HStack></HStack>
+                </HStack>
+            </Box>
     );
 };
 

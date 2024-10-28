691abf9f880b6337a02869e5390fb574a2113352
Faith-Nchang <fnchang@montgomerycollege.edu>
2024-10-28 14:48:28 -0400

    CU-8696a0258 updated contact us

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import { Provider } from "./components/ui/provider";
 import Header from './views/layout/Header';
 import ContactUs from './components/ContactUs';
 
+import HowItWorks from './pages/HowItWorks/HowItWorks';
 
 function App() {
   return (
     <Provider>
       <Header />
+     
+      
+      <HowItWorks />
+
       <ContactUs />
     </Provider>
   );
--- src/components/navbar/RightContent.js
+++ src/components/navbar/RightContent.js
@@ -1,6 +1,29 @@
-function RightContent() {
+import { Button, Flex, Link } from '@chakra-ui/react';
+
+function RightContent({navItems}) {
+
     return (
-        <p>Right Content</p> 
+        <Flex alignItems='center' justifyContent='center' gap={4} mr={20}>
+            {navItems?.map((item) => {
+                return (
+                   <Link
+                        href={item.href}
+                   >
+                        <Button 
+                            key={item.name}
+                            color='white'
+                            textDecoration='none'
+                            border='none'
+                            bg='transparent'
+                            _hover={{background: 'blackAlpha.300'}}
+
+                    >
+                        <strong>{item.name}</strong>
+                    </Button>
+                   </Link> 
+                );
+            })}
+        </Flex>
     );
 }
 

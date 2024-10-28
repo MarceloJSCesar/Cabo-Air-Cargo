a01cf37fb143f234cf8326ca2ee68fb7a717b53b
Faith Nchang <143855102+Faith-Nchang@users.noreply.github.com>
2024-10-28 16:28:22 -0400

    Update ContactUs.jsx

--- src/components/ContactUs.jsx
+++ src/components/ContactUs.jsx
@@ -1,37 +1,44 @@
-import { FaLinkedin, FaInstagram, FaFacebook, FaMapMarkerAlt } from 'react-icons/fa';
+import { FaLinkedin, FaInstagram, FaFacebook, FaMapMarkerAlt, FaEnvelope } from 'react-icons/fa';
 import './ContactUs.css';
-import LocationMap from './Map';
 
 const ContactUs = () => {
     return (
-        <div className="container">
+        <div className="contact_container">
             <div className="contacts">
                 <h1>Contact Us</h1>
-                <h2>Where You Can Find <span>Us</span></h2>
+                <h2>Where Can You <span className="find">Find <span>Us</span></span></h2>
                 <div className="socials">
                     <div className="item">
-                        <FaMapMarkerAlt className="icon" size={30} />
+                        <FaMapMarkerAlt className="icon" size={40} />
                         <p>1234 Street Name, US</p>
                     </div>
                     <div className="item">
-                        <FaFacebook className="icon" size={30} />
+                        <FaFacebook className="icon" size={40} />
                         <a href="#">Carbo Air Cargo</a>
                     </div>
-                    <div className="item item-3">
-                        <FaLinkedin className="icon" size={30} />
-                        <a href="#">Carbo Air Cargo LinkedIn</a>
+                    <div className="item">
+                        <FaLinkedin className="icon" size={40} />
+                        <a href="https://www.linkedin.com/company/prendi-cv/">LinkedIn</a>
                     </div>
-                    <div className="item item-4">
-                        <FaInstagram className="icon" size={30} />
+                    <div className="item">
+                        <FaInstagram className="icon" size={40} />
                         <a href="#">Carbo Air Cargo Instagram</a>
                     </div>
+
+                    <div className="mail-us item">
+                    <a href="mailto:info@carboaircargo.com" className="mail-button">
+                        Mail Us
+                        <FaEnvelope className="mail-icon" />
+                    </a>
                 </div>
+                </div>
+               
             </div>
             <div className='map'>
-                <img src="map.png" alt="map" />
+                <img src="map.png" alt="map" className="mapImg"/>
             </div>
         </div>
     );
-}
+};
 
 export default ContactUs;

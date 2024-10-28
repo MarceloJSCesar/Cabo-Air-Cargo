4b761dc0af5cc0630f8ed7ddd73c96176ae20e4c
Liedson Correia <liedsoncanal@gmail.com>
2024-10-28 20:16:20 +0000

    CU-8696cedvb: connect application to external API

--- src/views/sections/HowItWorks/HowItWorks.js
+++ src/views/sections/HowItWorks/HowItWorks.js
@@ -1,8 +1,27 @@
+import { useState, useEffect} from 'react';
+import  axios  from 'axios';
 import VideoPlayer from "../../../components/VideoPlayer/VideoPlayer"
 import "./style.css"
 
 export default function HowItWorks(){
 
+
+    const [data, setData] = useState({});
+
+    useEffect(() => {
+        axios.get('https://youtube.com')
+            .then(response => setData(response.data))
+            .catch(() => {
+                setData({
+                    title: "How It Works",
+                    subtitle: "Receive your packages",
+                    span: "faster",
+                    content: "Watch this simple step by step video, and see how you can receive all your orders from anywhere quicker than ever!",
+                    videoTitle: "Video Title",
+                    url: "https://www.youtube.com/embed/tgbNymZ7vqY",
+                });
+            });
+    }, []);
 
     return (
         <div className="howitworks" id="howitworks">
@@ -11,23 +30,21 @@ export default function HowItWorks(){
                 <div className="hwrapper">
 
                     <div className="Headers">
-                        
-                        <h1 className="title">How It Works</h1>
-                        <p className="subtitle">Receive your<br/>
-                        packages <span>faster</span> </p>
+                        <h1 className="title">{data.title}</h1>
+                        <p className="subtitle">{data.subtitle}&nbsp;<span>{data.span}</span> </p>
                     </div>
 
                     <div className="content">
                         <p>
-                        Watch this simple step by step video, and see how you can receive all your orders from anywhere quicker than ever!
+                        {data.content}
                         </p>
                     </div>
 
                 </div>
                 <div className="container">
                 <VideoPlayer
-                title="Video player"
-                src="https://www.youtube.com/embed/tgbNymZ7vqY"
+                title={data.videoTitle}
+                src={data.url}
                 className="videoplayer"
                 />
                 </div>

de0898056d7dcd0d8beb3cdc0a2a35c69f67864b
azilrababe <azilrababe@gmail.com>
2024-11-01 10:07:05 +0100

    feat: add APIs for dynamic home page content updates

--- backend/controllers/aboutUs.js
+++ backend/controllers/aboutUs.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 import AboutUs from "../models/aboutUs.js";
 
-// create aboutUs
-router.post("/", async (req, res) => {
-  const newAboutUs = new AboutUs(req.body);
+router.post("/aboutus", async (req, res) => {
+  const { title, rightTxt, leftTxt } = req.body;
+
+  const newAboutUs = new AboutUs({ title, rightTxt, leftTxt });
   try {
     const savedAboutUs = await newAboutUs.save();
     res.status(200).json(savedAboutUs);
@@ -14,8 +15,7 @@ router.post("/", async (req, res) => {
   }
 });
 
-// read aboutUs
-router.get("/", async (req, res) => {
+router.get("/aboutus", async (req, res) => {
   try {
     const aboutUs = await AboutUs.find();
     res.status(200).json(aboutUs);
@@ -24,32 +24,4 @@ router.get("/", async (req, res) => {
   }
 });
 
-// update aboutUs
-router.put("/:id", async (req, res) => {
-  try {
-    const aboutUs = await AboutUs.findById(req.params.id);
-    if (aboutUs) {
-      await aboutUs.updateOne({ $set: req.body });
-      res.status(200).json("The aboutUs has been updated");
-    } else {
-      res.status(404).json("aboutUs not found");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
-
-// delete aboutUs
-router.delete("/:id", async (req, res) => {
-  try {
-    const aboutUs = await AboutUs.findById(req.params.id);
-    if (aboutUs) {
-      await aboutUs.deleteOne();
-      res.status(200).json("The aboutUs has been deleted");
-    } else {
-      res.status(404).json("aboutUs not found");
-    }
-  } catch (err) {
-    res.status(500).json(err);
-  }
-});
+export default router;

0eb5da91d279505bb74f411819258a3affb543fb
Srikar Sannidhi <sriksan@unc.edu>
2024-11-07 01:05:52 -0500

    CU-8696du47u

--- src/views/sections/Footer/Footer.js
+++ src/views/sections/Footer/Footer.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import './styles.css';
-//import logo from '../../assets/default/logo.png';
+import logo from '../../../assets/default/logo.png';
 import { FaInstagram, FaLinkedin, FaFacebook, FaPaperPlane } from 'react-icons/fa';
 
 const Footer = () => {
   return (
 	<footer className="footer">
 		<div className="footer-logo">
-			<img src="logo" alt="Logo" className="footer-logo-image" />
-			<h3>CABO AIR CARGO</h3>
-			<p>Lorem ipsum dolor sit amet consectetur. Eros et aliquam amet aliquet vivamus convallis elit pellentesque</p>
-			<div className="footer-social-logos">
-				<FaInstagram />
-				<FaLinkedin />
-				<FaFacebook />
+			<div className="footer-logo-container">
+				<img src={logo} alt="Logo" className="footer-logo-image" />
+				<h3>CABO AIR CARGO</h3>
 			</div>
+				<p className='footer-description'>Lorem ipsum dolor sit amet consectetur. Eros et aliquam amet aliquet vivamus convallis elit pellentesque</p>
+				<div className="footer-social-logos">
+					<FaInstagram />
+					<FaLinkedin />
+					<FaFacebook />
+				</div>
 	  </div>
 	  <div className="footer-links">
 		<h3>LINKS</h3>

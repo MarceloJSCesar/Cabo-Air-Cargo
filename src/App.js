980a292843bb9d51bdc1a96823b6f2d3bda4754b
Liedson Correia <liedsoncanal@gmail.com>
2024-11-18 20:56:03 +0000

    feat-8696pvtdf: add 404 page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 import { Provider } from "./components/ui/provider";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './views/layout/Header';
-import HowItWorks from './views/sections/HowItWorks/HowItWorks';
-import ContactUs from './components/ContactUs'; // Keep the import from feat-CU-8696a0258
-import AboutUs from './views/sections/AboutUs/AboutUs';
+import NotFound from './views/NotFound';
+import Pages from './views/Pages';
 
 
 function App() {
   return (
     <Provider>
-      <Header />
-      <HowItWorks />
-      <AboutUs />
-      <ContactUs /> 
+      <Router>
+        <Routes>
+          <Route path="/" element={<Pages />} />
+          <Route path="*" element={<NotFound />} /> {/* Catch-all route */}
+        </Routes>
+      </Router>
     </Provider>
   );
 }

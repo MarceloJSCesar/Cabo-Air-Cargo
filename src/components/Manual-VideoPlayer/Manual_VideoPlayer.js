83ea5edac7abb5bc354c5557f7bb84ddb8951ed5
Marcelo <jsdevmarcelo@gmail.com>
2024-11-20 18:08:21 -0100

    replay button emoved

--- src/components/Manual-VideoPlayer/Manual_VideoPlayer.js
+++ src/components/Manual-VideoPlayer/Manual_VideoPlayer.js
@@ -27,17 +27,6 @@ const ManualVideoPlayer = () => {
         <source src={video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      
-      {hasPlayed && (
-        <button
-          onClick={handleReplay}
-          className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-                     bg-white/80 hover:bg-white px-4 py-2 rounded-full shadow-md
-                     transition-all duration-300 ease-in-out"
-        >
-          Replay
-        </button>
-      )}
     </div>
   );
 };

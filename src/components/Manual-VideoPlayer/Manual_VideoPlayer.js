be10aceee1b4b4eb7751773ba6dbb31a688236e8
Marcelo <jsdevmarcelo@gmail.com>
2024-11-20 18:11:54 -0100

    border added

--- src/components/Manual-VideoPlayer/Manual_VideoPlayer.js
+++ src/components/Manual-VideoPlayer/Manual_VideoPlayer.js
@@ -1,5 +1,6 @@
 import React, { useRef, useState } from 'react';
-import video from '../../assets/video-how-it-works.mp4'
+import video from '../../assets/video-how-it-works.mp4';
+import './manual-videoplayer.css';
 
 const ManualVideoPlayer = () => {
   const videoRef = useRef(null);
@@ -16,7 +17,7 @@ const ManualVideoPlayer = () => {
   };
 
   return (
-    <div className="relative w-full max-w-3xl mx-auto my-8">
+    <div className="manual-video-player relative w-full max-w-3xl mx-auto my-8">
       <video
         ref={videoRef}
         className="w-full rounded-lg shadow-lg"

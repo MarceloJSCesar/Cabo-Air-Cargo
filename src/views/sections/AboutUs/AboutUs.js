9fae5de2a8b501c06c2497833d60757fb41faa32
Marcelo <jsdevmarcelo@gmail.com>
2024-11-21 19:11:40 +0000

    about us fixed: text underneath each other

--- src/views/sections/AboutUs/AboutUs.js
+++ src/views/sections/AboutUs/AboutUs.js
@@ -23,18 +23,11 @@ export default function AboutUs() {
                     <div>
 
                         <p>
-                            Additionally, our ultra competitive pricing will give every Cape Verdean access to e-Commerce products from United States, Europe, Africa, and South America and receive in Cape Verde in just 10 days or less. 
+                            Cape Verde Businesses will now be able to keep high quality products in inventory by utilizing our super fast air cargo services.
                         </p>
 
-                    </div>
-                    
-                    <div>
-                        {/* <h2 className="AboutUs-subtitle" aria-label="Our vision, values and Goals">
-                        Our <span className="AboutUs-span1">vision</span>, <span className="AboutUs-span2">values</span> and <span className="AboutUs-span1">Goals</span>
-                        </h2>    */}
-                        
                         <p>
-                            Cape Verde Businesses will now be able to keep high quality products in inventory by utilizing our super fast air cargo services.
+                            Additionally, our ultra competitive pricing will give every Cape Verdean access to e-Commerce products from United States, Europe, Africa, and South America and receive in Cape Verde in just 10 days or less. 
                         </p>
 
                     </div>

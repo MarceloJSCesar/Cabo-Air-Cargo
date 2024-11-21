e4cda2c52382cf922a0c5de6b23699ac2935c10d
Marcelo <jsdevmarcelo@gmail.com>
2024-11-21 20:11:50 +0000

    about us mobile version set as desktop

--- src/views/sections/AboutUs/AboutUs.js
+++ src/views/sections/AboutUs/AboutUs.js
@@ -8,13 +8,6 @@ export default function AboutUs() {
     return (
     <div className="AboutUs-section" id="about-us">
 
-                <div className="AboutUs-main">
-                    <h1 className="AboutUs-title" aria-label="About Us">ABOUT US</h1>
-                    {/* <h2 className="AboutUs-subtitle" aria-label="Our vision, values and Goals">
-                        Our <span className="AboutUs-span1">vision</span>, <span className="AboutUs-span2">values</span> and <span className="AboutUs-span1">Goals</span></h2> */}
-                    <p>Cape Verde Businesses will now be able to keep high quality products in inventory by utilizing our super fast air cargo services.</p>
-                </div>
-
             <div className="AboutUs-main-desktop">
                 <h1 className="AboutUs-title">
                     ABOUT US

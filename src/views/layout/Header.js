297c194db9fd6597d1e190e2e46753a4efccef63
Marcelo <jsdevmarcelo@gmail.com>
2024-11-25 10:02:51 +0000

    header image re-coded to be fully visible on mobile and desktop view

--- src/views/layout/Header.js
+++ src/views/layout/Header.js
@@ -1,4 +1,4 @@
-import { HStack, Box, VStack } from "@chakra-ui/react";
+import { HStack, Box } from "@chakra-ui/react";
 import RightContent from "../../components/navbar/RightContent";
 import LeftContent from "../../components/navbar/LeftContent";
 import img from "../../assets/header-img.jpg";
@@ -7,7 +7,6 @@ import MenuDrawer from "../../components/MenuDrawer/MenuDrawer";
 import './Header.css';
 
 function Header() {
-
     const navItems = [
         {
             name: 'Home',
@@ -28,50 +27,35 @@ function Header() {
     ];
 
     return (
-            
-            <Box
-                bgSize='cover'
-                bgPos='center'
-                bgRepeat='no-repeat'
-                h={{base: '65vh'}}
-                display='flex'
-                flexDirection='row'
-                alignItems='end'
-                alignContent='center'
-                justifyContent='center'
-                borderRadius='0px 0px 64px 64px'
-                id="home-section"
-                bg='#82A1C4'
-
+        <Box
+            position="relative"
+            id="home-section"
+            overflow="hidden"
+        >
+            <HStack 
+                h='10vh'
+                minW='100%' 
+                p={2} 
+                bg='transparent'
+                justifyContent='space-between' 
+                position='absolute'
+                top={0}
+                left={0}
+                zIndex={1000}
             >
-                <HStack 
-                    h='10vh'
-                    minW='100vw' 
-                    p={2} 
-                    bg='transparent'
-                    alignSelf='start' 
-                    justifyContent='space-between' 
-                    position='absolute'
-                    z-index='5000'
-                >
-                
-                    <LeftContent />
-                    
-                    <RightContent navItems={navItems}/>
-
-                    <p className="navbar-mobile-title" > Cabo Air Cargo </p>
-
-                    <HStack>
-                        <button className="menuButton">
-                            <MenuDrawer navItems={navItems} />
-                        </button>
-                    </HStack>
+                <LeftContent />
+                <RightContent navItems={navItems}/>
+                <p className="navbar-mobile-title">Cabo Air Cargo</p>
+                <HStack>
+                    <button className="menuButton">
+                        <MenuDrawer navItems={navItems} />
+                    </button>
                 </HStack>
+            </HStack>
 
-                <HomeSection img={img}/>
-
-            </Box>
+            <HomeSection img={img}/>
+        </Box>
     );
-};
+}
 
 export default Header;

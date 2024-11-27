03a5d5dea2502c8b83da2187fa6f491173eb1571
Marcelo <jsdevmarcelo@gmail.com>
2024-11-27 12:31:23 +0000

    merge changes fixed

--- src/views/Pages.js
+++ src/views/Pages.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import img from "../assets/default/plane.png";
-import HomeSection from './sections/HomeSection';
+import Header from './layout/Header';
 import HowItWorks from './sections/HowItWorks/HowItWorks';
 import ContactUs from '../components/ContactUs';
 import AboutUs from './sections/AboutUs/AboutUs';
+import Footer from './sections/Footer/Footer';
 export default function Pages() {
   return (
     <>
-      <HomeSection />
+      <Header />
       <HowItWorks />
       <AboutUs />
       <ContactUs /> 

fa887d90cb42fb63f9916cdcb8f87648ac216637
Marcelo <jsdevmarcelo@gmail.com>
2024-11-27 22:55:41 +0000

    header and footer added to pages

--- src/views/Pages.js
+++ src/views/Pages.js
@@ -1,17 +1,19 @@
 import React from 'react'
 import img from "../assets/default/plane.png";
-import HomeSection from './sections/HomeSection';
+import Header from './layout/Header';
 import HowItWorks from './sections/HowItWorks/HowItWorks';
 import ContactUs from '../components/ContactUs';
 import AboutUs from './sections/AboutUs/AboutUs';
+import Footer from './sections/Footer/Footer';
+
 export default function Pages() {
   return (
     <>
-        <HomeSection img={img}/>
+        <Header/>
         <HowItWorks />
         <AboutUs />
         <ContactUs />
-        
+        <Footer />
     </>
   )
 }
